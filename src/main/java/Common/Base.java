cbd75c8ae10e539880453340934a8f618117f051
A Akil Ganesh <akilganesh3@gmail.com>
2025-04-27 21:45:26 +0530

    Changes added

--- src/main/java/Common/Base.java
+++ src/main/java/Common/Base.java
@@ -86,17 +86,10 @@ public class Base {
 		return props;
 	}
 	
-	public Properties db_Prop() throws IOException {
-		FileReader reader=new FileReader(System.getProperty("user.dir")+"\\src\\main\\java\\Repository\\db_config.properties");
-        Properties props=new Properties();
-        props.load(reader);
-		return props;
-	}
-	
-	public Connection Database_Statement(Connection connection,Statement statement) throws Exception {
-		String url = db_Prop().getProperty("db.url");
-        String username = db_Prop().getProperty("db.username");
-        String password = db_Prop().getProperty("db.password");
+	public Connection Database_Statement(Connection connection,Statement statement) {
+		String url = "jdbc:oracle:thin:@localhost:1522:XE"; 
+        String username = "system";
+        String password = "Admin";
         try {
         	Class.forName("oracle.jdbc.driver.OracleDriver");
 
@@ -209,4 +202,5 @@ public class Base {
 		Response r = RestAssured.given().header("Cookie",a1).contentType(ContentType.JSON).body(n1).when().post(h);
 		System.out.println("Craeting command : "+r.getStatusCode());
 	}
+	
 }
